c0594a51c1f321ccefddbc14c09d0a0e5ee75aae
Asahi Kuroki <kurok011@umn.edu>
2024-06-27 21:30:31 -0500

    add ternary condition to menu-item-button.tsx so that it renders image with different css styles when collasped

--- features/layout/sidebar-navigation/menu-item-button.tsx
+++ features/layout/sidebar-navigation/menu-item-button.tsx
@@ -22,7 +22,15 @@ export function MenuItemButton({
     <li className={classNames(styles.listItem, className)}>
       <Button className={styles.anchor} onClick={onClick}>
         {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img className={styles.icon} src={iconSrc} alt={`${text} icon`} />{" "}
+        {!isCollapsed ? (
+          <img className={styles.icon} src={iconSrc} alt={`${text} icon`} />
+        ) : (
+          <img
+            className={styles.iconCollapsed}
+            src={iconSrc}
+            alt={`${text} icon`}
+          />
+        )}{" "}
         {!isCollapsed && text}{" "}
       </Button>
     </li>
