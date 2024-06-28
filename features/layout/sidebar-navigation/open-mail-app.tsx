f8ec8db4db6f6644897f5211f0c9f5d7aef75507
Asahi Kuroki <kurok011@umn.edu>
2024-06-28 15:15:22 -0500

    problem in ci/cd

--- features/layout/sidebar-navigation/open-mail-app.tsx
+++ features/layout/sidebar-navigation/open-mail-app.tsx
@@ -1,6 +1,8 @@
-export default function openMailApp() {
+const openMailApp = () => {
   const email = "support@prolog-app.com";
   const subject = "Support Request:";
   const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}`;
   window.location.href = mailtoLink;
-}
+};
+
+export default openMailApp;
