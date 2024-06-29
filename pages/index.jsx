8b4faffffc9ae4e240b314cbd089c9fe0de5319d
Ash Kuroki <65479968+kurokiasahi222@users.noreply.github.com>
2024-06-29 16:32:48 -0500

    add another img tag for mobile view (logo-large). This appears when t… (#6)
    * add another img tag for mobile view (logo-large). This appears when the view is in mobile and disappear when the view is in desktop. The original image will only show in desktop and disappear in mobile
    
    * add config/.next to .gitignore

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,7 +7,7 @@ const IssuesPage = () => {
       <header className={styles.header}>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/icons/logo-large.svg" alt="Prolog logo" />
-        <a href={Routes.projects}>Dashboard</a>
+        <a href={Routes.projects}>Open Dashboard</a>
       </header>
       <button
         className={styles.contactButton}
