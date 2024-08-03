7fad8b5bae34b6f32d9008621033142dd41203ba
Asahi Kuroki <kurok011@umn.edu>
2024-08-03 16:17:15 -0500

    include storybook static in gitignore

--- features/ui/button/button.tsx
+++ features/ui/button/button.tsx
@@ -1,9 +1,73 @@
-import { ButtonHTMLAttributes } from "react";
+// import { ButtonHTMLAttributes } from "react";
 import classNames from "classnames";
 import styles from "./button.module.scss";
 
-export function Button(props: ButtonHTMLAttributes<HTMLButtonElement>) {
+export enum ButtonSize {
+  sm = "sm",
+  md = "md",
+  lg = "lg",
+  xl = "xl",
+}
+
+export enum ButtonColor {
+  primary = "primary",
+  secondary = "secondary",
+  gray = "gray",
+  empty = "empty",
+  emptyGray = "empty-gray",
+  error = "error",
+  emptyError = "empty-erorr",
+}
+
+export enum ButtonState {
+  default = "default",
+  hover = "hover",
+  focused = "focused",
+  disabled = "disabled",
+}
+
+// export enum ButtonIcon {
+//   leading = "leading",
+//   trailing = "trailing",
+//   only = "only",
+// }
+
+type ButtonProps = {
+  children: React.ReactNode;
+  size?: ButtonSize;
+  color?: ButtonColor;
+  state?: ButtonState;
+  className?: string;
+  onClick?: () => void;
+  // icon?: ButtonIcon;
+};
+
+export function Button({
+  children,
+  size = ButtonSize.md,
+  color = ButtonColor.primary,
+  state = ButtonState.default,
+  className,
+  onClick,
+}: ButtonProps) {
   return (
-    <button {...props} className={classNames(styles.button, props.className)} />
+    <div
+      className={classNames(
+        styles.container,
+        styles[size],
+        styles[color],
+        styles[state],
+        className,
+      )}
+      onClick={onClick}
+    >
+      {children}
+    </div>
   );
 }
+
+// export function Button(props: ButtonHTMLAttributes<HTMLButtonElement>) {
+//   return (
+//     <button {...props} className={classNames(styles.button, props.className)} />
+//   );
+// }
