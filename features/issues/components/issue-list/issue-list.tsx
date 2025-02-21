38fe2009acb0ae68505a260c82e855712d40e886
Asahi Kuroki <kurok011@umn.edu>
2025-02-21 21:31:12 +0900

    Add loading indicator inside issue-list too

--- features/issues/components/issue-list/issue-list.tsx
+++ features/issues/components/issue-list/issue-list.tsx
@@ -4,6 +4,7 @@ import { useGetProjects } from "@features/projects";
 import { useGetIssues } from "../../api/use-get-issues";
 import { IssueRow } from "./issue-row";
 import styles from "./issue-list.module.scss";
+import { LoadingIndicator } from "@features/ui";
 
 export function IssueList() {
   const router = useRouter();
@@ -18,7 +19,7 @@ export function IssueList() {
   const projects = useGetProjects();
 
   if (projects.isLoading || issuesPage.isLoading) {
-    return <div>Loading</div>;
+    return <LoadingIndicator />;
   }
 
   if (projects.isError) {
