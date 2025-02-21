236e331d1d215bc3e7c4c83e3a386b1c2b55ee3a
Asahi Kuroki <kurok011@umn.edu>
2025-02-21 22:29:55 +0900

    Complete Cypress tests

--- features/ui/loading-indicator/loading-indicator.tsx
+++ features/ui/loading-indicator/loading-indicator.tsx
@@ -1,3 +1,8 @@
+// Solution 9: Add loading screen to project list
+// Added after watching the solution video.
+// I first added the loading-indicator directly inside project list but then I moved it to the features/ui folder.
+// This way the component is isolated and reusable.
+
 import Image from "next/image";
 import loadingCircle from "../../../public/icons/loading-circle.svg";
 import styles from "./loading-indicator.module.scss";
