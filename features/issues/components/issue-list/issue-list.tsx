3f1a5a69691772de4cc1fbaa3bcbe4f0e7b11e81
Ash (Asahi) Kuroki <65479968+kurokiasahi222@users.noreply.github.com>
2025-02-22 14:18:03 +0900

    Solution: Projects loading screen (#16)
    * Loading screen design for both mobile and desktop
    
    * Move loadingIndicator to UI folder to create a storybook component for reusability
    
    * Add loading indicator inside issue-list too
    
    * Complete Cypress tests

--- features/issues/components/issue-list/issue-list.tsx
+++ features/issues/components/issue-list/issue-list.tsx
@@ -4,6 +4,7 @@ import { useGetProjects } from "@features/projects";
 import { useGetIssues } from "../../api/use-get-issues";
 import { IssueRow } from "./issue-row";
 import styles from "./issue-list.module.scss";
+import { LoadingIndicator } from "@features/ui";
 
 export function IssueList() {
   const router = useRouter();
@@ -18,7 +19,7 @@ export function IssueList() {
   const projects = useGetProjects();
 
   if (projects.isLoading || issuesPage.isLoading) {
-    return <div>Loading</div>;
+    return <LoadingIndicator />;
   }
 
   if (projects.isError) {
@@ -41,7 +42,7 @@ export function IssueList() {
   const { items, meta } = issuesPage.data || {};
   // console.log(items);
   return (
-    <div className={styles.container}>
+    <div className={styles.container} data-testid="issue-list">
       <table className={styles.table}>
         <thead>
           <tr className={styles.headerRow}>
